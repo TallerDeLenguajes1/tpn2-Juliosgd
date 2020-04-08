04d55ec2f4055b04b2809a2a0eb2a15070e0b943
julio altamiranda <julioaltamiranda77@gmail.com>
2020-04-07 23:33:25 -0300

    Revert "punto 2a) (segundo commit)"
    This reverts commit 1c423335e8b1f14dcf68dc704e1cc6fd7b892273.

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -1,19 +1,13 @@
-#include <stdio.h>
 #define N 4
 #define M 5
-int main(){
-int f,c;
-double mt[N][M]={
-    {1,2,34,1,1},
-    {2,31,12,3,1},
-    {23,2,21,21,1},
-    {1,23,2,1,3},
-};
-for(f = 0;f<N; f++){
-    for(c = 0;c<M; c++){
-    printf("  %.f" , mt[f][c]);
-    }
-    printf("\n");
+Int f,c;
+Double mt[N][M];
+…
+for(f = 0;f<N; f++)
+{
+ for(f = 0;f<N; f++)
+{
+cprintf(“%lf ”, mt[f][c]);
 }
-    return 0;
+cprintf(“\n”);
 }
